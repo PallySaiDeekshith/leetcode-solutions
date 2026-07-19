3134ecf9025c638eae7a0dce4b9cd5972f7eb5e7
Sai Deekshith <saideekshithpally@gmail.com>
2026-07-19 18:46:01 +0530

    Time: 1 ms (99.9%), Space: 55.7 MB (46.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,15 +3,14 @@ class Solution {
         int c=1;
         int ele=nums[0];
         for(int i=1;i<nums.length;i++){
+            if(c==0){
+                ele=nums[i];
+            }
             if(nums[i]==ele){
                 c++;
             }
             else{
                 c--;
-            }
-            if(c==0){
-                c=1;
-                ele=nums[i];
             }
         }
         return ele;
