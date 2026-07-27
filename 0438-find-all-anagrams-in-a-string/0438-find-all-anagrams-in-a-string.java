a63937bce2ce727f7ea8c88282c4a587c43950a4
Sai Deekshith <saideekshithpally@gmail.com>
2026-07-27 19:35:06 +0530

    Time: 10 ms (80.82%), Space: 46.9 MB (46.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -3,29 +3,24 @@ class Solution {
         if(s.length()<p.length()){
             return new ArrayList<>();
         }
-        Map<Character,Integer> mp=new HashMap<>();
-        Map<Character,Integer> np=new HashMap<>();
-        for(int i=0;i<p.length();i++){
-            mp.put(p.charAt(i),mp.getOrDefault(p.charAt(i),0)+1);
-            np.put(s.charAt(i),np.getOrDefault(s.charAt(i),0)+1);
-        }
         List<Integer> ans=new ArrayList<>();
-        if(mp.equals(np)){
+        int[] freq=new int[26];
+        int[] window=new int[26];
+        for(int i=0;i<p.length();i++){
+            window[s.charAt(i)-'a']++;
+            freq[p.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(freq,window)){
             ans.add(0);
         }
         int l=0;
         int r=p.length()-1;
         while(r<s.length()-1){
-            if(np.get(s.charAt(l))==1){
-                np.remove(s.charAt(l));
-            }
-            else{
-                np.put(s.charAt(l),np.getOrDefault(s.charAt(l),0)-1);
-            }
+            window[s.charAt(l)-'a']--;
             l++;
             r++;
-            np.put(s.charAt(r),np.getOrDefault(s.charAt(r),0)+1);
-            if(mp.equals(np)){
+            window[s.charAt(r)-'a']++;
+            if(Arrays.equals(freq,window)){
                 ans.add(l);
             }
         }
